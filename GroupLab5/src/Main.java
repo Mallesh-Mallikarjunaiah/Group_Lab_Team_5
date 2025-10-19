8fcb977149d1b1ea00813e2c1f5155781719fb36
Mallesh MAllikarjunaiah <mallikarjunaiah.m@northeastern.edu>
2025-10-18 20:05:13 -0400

    Initial commit

---
+++ GroupLab5/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MALLESH
+ */
+public class Main {
+    public static void main(String[] args) {
+       Mallesh.main(args);
+       Gagana.main(args);
+       Jayanth.main(args);
+       Hameed.main(args);
+     }
+}
